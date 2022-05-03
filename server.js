c32f6ff3382b5c915113c47e59a3afa544c49e2c
maggiejoe <maggiejoseph94@gmail.com>
2022-05-03 16:08:28 -0400

    web server created

--- server.js
+++ server.js
@@ -45,6 +45,12 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray)
+ {
+     const result = animalsArray.filter(animal => animal.id === id)[0];
+     return result;
+ }
+
 app.get("/api/animals", (req, res) => {
     let results = animals;
     if (req.query) {
@@ -52,6 +58,15 @@ app.get("/api/animals", (req, res) => {
         console.log(results);
     }
     res.json(results);
+});
+
+app.get("/api/animals/:id", (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }   
 });
 
 // end of server.js
