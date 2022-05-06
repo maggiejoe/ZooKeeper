40359b8b1e5cdbca9014eb521744aa948789e5e3
maggiejoe <maggiejoseph94@gmail.com>
2022-05-05 20:53:26 -0400

    committing all files

--- server.js
+++ server.js
@@ -1,25 +1,18 @@
-// const fs = require('fs');
-// const path = require('path');
-// const { animals } = require("./data/animals.json");
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-
 const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.static('public'));
 
-// parse incoming string or array data
-app.use(express.urlencoded({ extended: true }));
-// parse incoming JSON data
-app.use(express.json());
-
+// Use apiRoutes
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
-// end of server.js
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-})
+  console.log(`API server now on port ${PORT}!`);
+});
